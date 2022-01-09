fbbabb6057ceda5a1a5cb0c97f8343cac730ec2b
Ewoskyx <ewoskyx@gmail.com>
2022-01-10 02:38:00 +0300

    Add deployment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./functionBased/App.css";
 // render
 render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </BrowserRouter>
   </React.StrictMode>,
