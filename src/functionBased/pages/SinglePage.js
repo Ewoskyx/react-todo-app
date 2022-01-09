bff2cd7437344fb144ea4c8b86035ff4d769af39
Ewoskyx <ewoskyx@gmail.com>
2022-01-10 02:47:39 +0300

    Update title and about page

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -13,7 +13,7 @@ const SinglePage = () => {
       slug: "about-author",
       title: "About the Author",
       description:
-        "This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.",
+        "This app was developed by Evren Ersoy, a Full Stack web developer student at Microverse while this project is published.  So go ahead and connect with Evren on Twitter @Ewoskyx.",
     },
   ];
 
